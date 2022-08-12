576d64a42e55bccc7d54e6bc9da037883b36c505
omartins-zs <gabrielmatheus209@gmail.com>
2022-08-12 12:52:27 -0300

    Comentando Teste

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-const MyNameApp = {
-  data() {
-    return {
-      name: "Matheus",
-      idade: 30,
-    };
-  },
-};
-
-Vue.createApp(MyNameApp).mount("#app");
+// const MyNameApp = {
+//   data() {
+//     return {
+//       name: "Matheus",
+//       idade: 30,
+//     };
+//   },
+// };
+// Vue.createApp(MyNameApp).mount("#app");
