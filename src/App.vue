ebb8f165dd2355584045ff54e43023d1e7e9443f
omartins-zs <gabrielmatheus209@gmail.com>
2022-08-12 16:25:46 -0300

    Iniciando metodos e detalhes de um Pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,54 @@
 <template>
   <v-app>
     <v-container>
+      <v-text-field v-model="search" label="Pesquisar" placeholder="Placeholder" solo>
+      </v-text-field>
       <v-card>
         <v-container>
           <!-- {{ pokemons }} -->
-          <v-row>
-            <v-col cols="3" v-for="pokemon in pokemons.slice(0, 10)" :key="pokemon.name">
-              <v-card>
+          <v-row class="mx-0 d-flex justify-center">
+            <v-col cols="3" v-for="pokemon in filtered_pokemons" :key="pokemon.name">
+              <v-card @click="show_dialog = !show_dialog">
                 <v-container>
-                  <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(pokemon)}.png`" :alt="pokemon.name" srcset="" width="80%">
+                  <img
+                    :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(pokemon)}.png`"
+                    :alt="pokemon.name" srcset="" width="80%">
                   <h2 class="text-center">{{ pokemon.name }}</h2>
                 </v-container>
               </v-card>
             </v-col>
           </v-row>
         </v-container>
-      </v-card>
     </v-container>
+    <div class="text-center">
+     
+      <v-dialog v-model="show_dialog" width="800">
+          <v-container>
+        <v-card @click="show_pokemon(get_id(pokemon))">
+          <v-card-title class="text-h5 grey lighten-2">
+            Privacy Policy
+          </v-card-title>
+
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="dialog = false">
+              I accept
+            </v-btn>
+          </v-card-actions>
+          </v-container>
+        </v-card>
+      </v-dialog>
+    </div>
   </v-app>
 </template>
 
@@ -32,7 +64,10 @@ export default {
 
   data: () => {
     return {
-      pokemons: []
+      pokemons: [],
+      search: "",
+      show_dialog: false,
+      selected_pokemon: null
     }
   },
 
@@ -45,10 +80,29 @@ export default {
       })
   },
 
-  methods:{
-    get_id(pokemon){
-    return Number(pokemon.url.split("/")[6]);
-  }}
+  methods: {
+    get_id(pokemon) {
+      return Number(pokemon.url.split("/")[6]);
+    },
+    get_name(pokemon) {
+      return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    },
+    show_pokemon(id){
+      axios.get("https://pokeapi.co/api/v2/pokemon/${id}")
+      .then((response) => {
+        this.selected_pokemon = response.data;
+        // console.log(response);
+      this.show_dialog = !this.show_dialog
+      })
+    }
+  },
+  computed: {
+    filtered_pokemons() {
+      return this.pokemons.filter((item) => {
+        return item.name.includes(this.search);
+      })
+    },
+  },
 };
 </script>
 <style>
